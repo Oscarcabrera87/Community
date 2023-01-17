8510182b40be6625b9c2740c7786c8d8030af02a
Oscarcabrera87 <Oscarcabrera87@gmail.com>
2023-01-16 20:40:34 -0500

    Finished the 4th video

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import Employee from "./components/Employee"
+import { useState } from "react";
 
 function App() {
+  const [role, setRole] = useState('dev')
   const showEmployees = true
+  
   return (
     <div className="App">
       {showEmployees ?
       <>
+      <input
+        type='text'
+        onChange={(e) => {
+          console.log(e.target.value)
+          setRole(e.target.value)
+        }} 
+      />
        <Employee name="Caleb" role="intern"/>
        <Employee name="Jacob" />
-       <Employee name="Mark" role="intern"/>
+       <Employee name="Mark" role={role}/>
        <Employee name="Samantha" role="Admin"/>
        </>
        : <p>
