db747a35e42c8e12d4b1070fe68d48e9c297c3ea
Oscarcabrera87 <Oscarcabrera87@gmail.com>
2023-01-16 20:17:20 -0500

    Finished second video

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
+import Employee from "./components/Employee"
 
 function App() {
+  const showEmployees = true
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ?
+      <>
+       <Employee />
+       <Employee />
+       <Employee />
+       <Employee />
+       <Employee />
+       </>
+       : <p>
+        You can not see the employees
+       </p>
+      }
     </div>
   );
 }
